595c4347c2576670c96a42b4a9b9eb9c2729ef6b
Tarek Taha <tarek.taha@gmail.com>
2016-05-20 16:06:32 +0400

    - test update

--- updateCode.py
+++ updateCode.py
@@ -56,6 +56,6 @@ while True:
     # Step 2: kill the script
     killProcessByName('babyMonitor.py')
     # Step 3: re-run the script
-    subprocess.Popen(["babyMonitor.py `cat apiId.txt`"])
+    subprocess.Popen("babyMonitor.py `cat apiId.txt`")
     restartProgram()
   time.sleep(delayTime)
